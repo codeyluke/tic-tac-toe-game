6c4307c77211ae8ca75a7722cced310996add865
Luke Sia <lukekennedy212@gmail.com>
2018-12-11 18:28:18 +0800

    remove the movesMade

--- script.js
+++ script.js
@@ -2,13 +2,16 @@ const squares = document.querySelectorAll(".square");
 const result = document.querySelector("#result");
 const player1 = `<i class="fas fa-times"></i>`;
 const player2 = `<i class="far fa-circle"></i>`;
+const comp = `<i class="fas fa-desktop"></i>`;
 
 //Initialiser
 let currentTurn = true;
 let movesMade = 0;
 let player1Data = [];
 let player2Data = [];
-let compData = [];
+let compAI = false;
+let comData = [];
+let winner = false;
 const winningCombos = [
   [0, 1, 2],
   [3, 4, 5],
@@ -23,16 +26,59 @@ const winningCombos = [
 /*Restart the game*/
 function restart() {
   for (let i = 0; i < squares.length; i++) {
-    squares[i].classList.remove("disabled");
-    squares[i].classList.remove("o");
-    squares[i].classList.remove("x");
+    squares[i].classList.remove("disabled", "taken");
     squares[i].innerHTML = "";
     result.innerHTML = `<h5>Who will win?</h5>`;
   }
   movesMade = 0;
   currentTurn = true;
+  winner = false;
   player1Data = [];
   player2Data = [];
+  comData = [];
+}
+
+/*Choosing the player or AI*/
+
+/*Players or AI Functions*/
+function firstPlayer(e, square) {
+  e.target.innerHTML = player1;
+  currentTurn = false;
+  movesMade++;
+  //this is to prevent player from reselecting the same square
+  square.classList.add("taken", "disabled");
+  player1Data.push(square.id);
+}
+
+function secPlayer(e, square) {
+  e.target.innerHTML = player2;
+  currentTurn = true;
+  movesMade++;
+  //this is to prevent player from reselecting the same square
+  square.classList.add("taken", "disabled");
+  player2Data.push(square.id);
+}
+
+function comPlayer() {
+  //put the squares that has not been chosen yet inside notChosen
+  //randomly choose inside the array
+  let notChosen = [];
+
+  for (let i = 0; i < squares.length; i++) {
+    if (!squares[i].className.includes("taken")) {
+      notChosen.push(squares[i]);
+    }
+  }
+
+  let random = Math.floor(Math.random() * notChosen.length);
+  let compChosen = notChosen[random];
+  //this is to prevent player from reselecting the same square
+  if (notChosen.length > 0) {
+    compChosen.classList.add("taken", "disabled");
+    compChosen.innerHTML = comp;
+    comData.push(compChosen.id);
+    movesMade++;
+  }
 }
 
 /*
@@ -43,60 +89,81 @@ compare to the winning arrays
 */
 for (let i = 0; i < squares.length; i++) {
   squares[i].onclick = function(e) {
-    if (currentTurn) {
-      e.target.innerHTML = player1;
-      currentTurn = false;
-      player1Data.push(squares[i].id);
-      console.log(player1Data);
-    } else {
-      e.target.innerHTML = player2;
+    if (currentTurn && compAI) {
+      firstPlayer(e, squares[i]);
+      comPlayer();
+
       currentTurn = true;
-      player2Data.push(squares[i].id);
-      console.log(player2Data);
+    } else if (currentTurn) {
+      firstPlayer(e, squares[i]);
+    } else {
+      secPlayer(e, squares[i]);
     }
-    //this is to prevent player from reselecting the same square
-    e.target.classList.add("disabled");
-    movesMade++;
+
     console.log(movesMade);
     //checking for winner or tie
-    if (movesMade > 4) {
-      checkWinner(movesMade, player1Data, player2Data);
-    }
+
+    checkWinner(movesMade, player1Data, player2Data, comData);
   };
 }
 
 //check winner after 4 moves then
 //tie at the 9 moves if no winner
-function checkWinner(movesMade, player1Data, player2Data) {
+function checkWinner(movesMade, player1Data, player2Data, comData) {
   //make the strings into num to be compared with the winningCombos
 
-  if (movesMade < 9) {
+  if (movesMade < 9 && winner == false) {
     let firstDataNum = player1Data.map(num => parseInt(num));
     let secDataNum = player2Data.map(num => parseInt(num));
+    let comDataNum = comData.map(num => parseInt(num));
 
     for (let i = 0; i < winningCombos.length; i++) {
+      if (winner) {
+        break;
+      }
       if (
         firstDataNum.includes(winningCombos[i][0]) &&
         firstDataNum.includes(winningCombos[i][1]) &&
         firstDataNum.includes(winningCombos[i][2])
       ) {
         //debugger;
+        winner = true;
+        stopGame();
         result.innerHTML = `<h5>The winner is Player 1</h5>`;
-        //console.log("PLAYER 1 WON");
       } else if (
         secDataNum.includes(winningCombos[i][0]) &&
         secDataNum.includes(winningCombos[i][1]) &&
         secDataNum.includes(winningCombos[i][2])
       ) {
+        winner = true;
         result.innerHTML = `<h5>The winner is Player 2</h5>`;
-        //console.log("PLAYER 2 WON");
+      } else if (
+        comDataNum.includes(winningCombos[i][0]) &&
+        comDataNum.includes(winningCombos[i][1]) &&
+        comDataNum.includes(winningCombos[i][2])
+      ) {
+        winner = true;
+        stopGame();
+        result.innerHTML = `<h5>The winner is the Computer</h5>`;
       }
     }
-
-    console.log(firstDataNum);
-    console.log(secDataNum);
   } else {
+    stopGame();
     result.innerHTML = `<h5>It's a tie</h5>`;
-    console.log("Its a tie");
+  }
+}
+
+/* STOP THE GAME */
+function stopGame() {
+  let notDisabled = [];
+
+  for (let i = 0; i < squares.length; i++) {
+    if (!squares[i].className.includes("disabled")) {
+      notDisabled.push(squares[i]);
+    }
+  }
+
+  for (let i = 0; i < notDisabled.length; i++) {
+    notDisabled[i].classList.add("disabled");
   }
 }
